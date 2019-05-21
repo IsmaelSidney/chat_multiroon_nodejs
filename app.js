1949986aeb2167adf00c3b638f2e3b37f3e86cf3
ismael sidney <ismaelsidney@gmail.com>
2019-05-21 20:08:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = require('./config/server');
+
+var server = app.listen(process.env.PORT || 80, function () {
+    console.log("server on");
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', function (socket) {
+    console.log('user connected');
+
+
+    socket.on('disconnect', function () {
+        console.log("user disconected");
+    });
+
+    socket.on('msgParaServidor', function (data) {
+
+        socket.emit(
+            'msgParaCliente', {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+        socket.broadcast.emit(
+            'msgParaCliente', {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente', {
+                    apelido: data.apelido
+                }
+            );
+            socket.broadcast.emit(
+                'participantesParaCliente', {
+                    apelido: data.apelido
+                }
+            );
+
+        }
+
+    });
+});
---
+++ app/controllers/chat.js
@@ -0,0 +1,21 @@
+module.exports.initChat = function  (application,req , res) {
+    var dadosForm = req.body;
+    
+    req.assert("apelido","apelido is required").notEmpty();
+    req.assert("apelido","nome ou apelido deve conter entre 3 e 15 caracteres").len(3,15);
+
+    var erros = req.validationErrors();
+
+    if (erros){
+        res.render("index",{validacao : erros});
+        return;
+    }
+
+    application.get('io').emit(
+        "msgParaCliente",
+        {apelido: dadosForm.apelido, mensagem:'acabou de entrar no chat'}
+        );
+    res.render('chat',{dadosForm:dadosForm});
+
+
+}
